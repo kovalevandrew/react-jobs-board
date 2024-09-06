70ef0f04056a30f6c743092cbc0a30ea45998c0a
Andrew Kovalov <andrewbubblegum@gmail.com>
2024-09-06 17:16:45 +0300

    continue react course, jobs page start

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,8 @@
 import Jobs from '../jobs.json' 
 import JobListing from './JobListing.jsx'
 
-const JobListings = () => {
-    const recentJobs = Jobs.slice(0,3);
+const JobListings = ( {isHome = false}) => {
+    const jobListings = isHome ? Jobs.slice(0,3): Jobs;
     return (
           <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,7 +10,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job) => (
+            {jobListings.map((job) => (
                 <JobListing key={job.id} job={job}/>
             )) }
         </div>
