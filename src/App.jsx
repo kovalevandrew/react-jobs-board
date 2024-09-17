37cd4ed449c6fc49a5ca08cf77ea2f1f834efb6c
Andrew Kovalov <andrewbubblegum@gmail.com>
2024-09-17 16:56:29 +0300

    job page + job page add form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
+import JobPage, {jobLoader} from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements (
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJobPage />} />
+    <Route path='/job/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
 )
